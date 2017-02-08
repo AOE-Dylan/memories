d5f8f625ca049d8d1597f6df1e0e64299c721725
AOE-Dylan <dylantt2000@gmail.com>
2017-02-08 09:22:47 -1000

    code doesnt work

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
 console.log("Sanity Check");
 
+var images = ["url('./css/assets/trollFace1.jpg')", "url('./css/assets/trollFace2.png')", "url('./css/assets/trollFace3.png')", "url('./css/assets/trollFace4.jpg')"];
+
 function background(card) {
-  var images = ["url('./css/assets/trollFace1.jpg')", "url('./css/assets/trollFace2.png')", "url('./css/assets/trollFace3.png')", "url('./css/assets/trollFace4.jpg')"];
-  document.getElementById(card).style.backgroundImage = images[Math.floor((Math.random() * 4))];
+  document.getElementById(card).style.backgroundImage = images[Math.floor(Math.random() * images.length)];
+  if (getElementById(card).style.backgroundImage == images[0]) {
+    images.splice(0, 1)
+  } else if (getElementById(card).style.backgroundImage == images[1]) {
+    images.splice(1, 1)
+  } else if (getElementById(card).style.backgroundImage == images[2]) {
+    images.splice(2, 1)
+  } else if (getElementById(card).style.backgroundImage == images[3]) {
+    images.splice(3, 1)
+  }
 }
 
+  
+
+  
